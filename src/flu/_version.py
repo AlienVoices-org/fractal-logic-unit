2c904750798be70857bed326110021a9ebcaf73e
AlienVoices <alienvoices@googlemail.com>
2026-03-26 13:58:52 +0100

    Update _version.py / V15.3.1

--- src/flu/_version.py
+++ src/flu/_version.py
@@ -1,5 +1,5 @@
-__version__       = "15.3.0"
-__build_date__    = "2026-03-12"
+__version__       = "15.3.1"
+__build_date__    = "2026-03-26"
 
 FLU_VERSION       = __version__
 FLU_VERSION_LABEL = f"FLU v{__version__} (build {__build_date__})"
